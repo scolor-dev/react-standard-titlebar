4e35c4486b3f2c7e2feda3243f7b90a6cb892567
scolor-dev <221649086+scolor-dev@users.noreply.github.com>
2025-07-24 05:20:43 +0900

    Remove ignored folders from repo

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Titlebar } from './components/Titlebar';
-export { ThemeProvider } from './context/ThemeProvider';
-export type { ThemeProviderProps } from './context/ThemeProvider';
-export type { TitlebarProps } from './components/Titlebar';
-import './styles/index.scss';
